149e8caa9d222476552fd40c3517c30953758cf5
maziogra <104913994+maziogra@users.noreply.github.com>
2025-11-03 17:50:34 +0100

    Allow to create a basic server

---
+++ include/server/ServerHTTP.h
@@ -0,0 +1,37 @@
+#ifndef SERVERHTTP_H
+#define SERVERHTTP_H
+#include <socket/Socket.h>
+
+#include "socket/BaseSocket.h"
+
+
+namespace maziogra_http {
+    class ServerHTTP {
+    protected:
+        std::unique_ptr<Socket> s;
+        int port;
+        // TODO map of routes and list of middlewares
+        // 0 no 1 yes
+        int secure;
+    public:
+        ServerHTTP(int secure) : secure(secure) {
+            if (secure) {
+                // s = std::make_unique<SecureSocket>(); // quando la implementerai
+            } else
+                s = std::make_unique<BaseSocket>();
+        }
+        ~ServerHTTP() {
+            s->close();
+        };
+
+        void start(int port);
+        // TODO fix this methods, these are not completed
+        // void addRoute();
+        // void removeRoute();
+        // void addMiddleware();
+        // void removeMiddleware();
+    };
+}
+
+
+#endif //SERVERHTTP_H
---
+++ src/Server/ServerHTTP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <server/ServerHTTP.h>
+
+namespace maziogra_http {
+    void ServerHTTP::start(int port) {
+        s->create(port);
+        while (true) {
+            auto client = s->accept();
+            if (client == nullptr) {continue;}
+            char* buffer = new char[1024];
+            client->receive(buffer, 1024);
+            std::cout << buffer << std::endl;
+            client->close();
+        }
+    }
+
+}
